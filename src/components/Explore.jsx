cb9b964d06ec15467a6745b3208c38d834b4793b
Oluwabukola-G <amoooluwabukola@outlook.com>
2024-10-21 18:43:02 +0100

    added footer

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -3,6 +3,7 @@ import { homeproduct } from "../db"; // assuming this contains filter options
 import { MdTune } from 'react-icons/md';
 import { FaChevronDown, FaSearch } from 'react-icons/fa';
 import { MdAdd } from 'react-icons/md';
+import { Link } from "react-router-dom";
 
 const Explore = () => {
 
@@ -24,7 +25,7 @@ const Explore = () => {
           <input 
             type="text" 
             placeholder="Search your preference" 
-            className=" pl-10 w-full rounded-lg border " 
+            className=" pl-10 w-full rounded-3xl border shadow-lg  border-none " 
           />
           <div className="absolute top-3 left-2 pl-2">
             <FaSearch />
@@ -34,7 +35,7 @@ const Explore = () => {
         <div className="relative lg:w-[20%]">
           <button 
             type="button" 
-            className="w-full flex items-center justify-between border rounded-lg px-3 py-2"
+            className="w-full flex items-center justify-between border rounded-3xl shadow-lg px-3 py-2"
             onClick={() => setShowDropdown(!showDropdown)}
           >
             <div className="flex items-center">
@@ -60,10 +61,11 @@ const Explore = () => {
           )}
         </div>
       </form>
+<div className="flex flex-col">
 
-      <h1 className="lg:text-3xl font-bold">Recommended Just for You</h1>
+<h1 className="lg:text-3xl font-bold">Recommended Just for You</h1>
      {/*featured products  */}
-     <section className="grid grid-cols-1 lg:ml-5 xl:ml-0  sm:grid-cols-2 sm:gap-[1rem] lg:grid-cols-3 mt-2 gap-[20px] md:gap-[1rem] lg:gap-[1.5rem] xl:gap-[3rem] ">
+     <section className="grid grid-cols-1    sm:grid-cols-2 sm:gap-[1rem] lg:grid-cols-3 mt-2 gap-[20px] md:gap-[1rem] lg:gap-[1.5rem] xl:gap-[3rem] ">
      
             {homeproduct.map((product) => {
               const { id, image, title, price,category } = product;
@@ -72,7 +74,10 @@ const Explore = () => {
                   key={id}
                   className="bg-white rounded-3xl border border-b-2  "
                 >
+                  <div>
                   <img src={image} alt={title} className="h-[400px] w-full rounded-3xl" />
+                  </div>
+            
                   <h2 className="text-[16px] font-bold text-start ml-5 mt-2">
                     {title}
                   </h2>
@@ -84,12 +89,17 @@ const Explore = () => {
                     </p>
                   </div>
         
-                              <button className='btn bg-slate-200 shadow-lg w-[50%] flex  p-2 text-black rounded-2xl mx-[19px] mb-[18px]  mt-2'> <MdAdd className="text-2xl text-[#df0c12]"/>Add to cart</button>
-
+                              <button className='btn bg-slate-200 shadow-lg w-[50%] flex  p-2 text-black rounded-3xl mx-[19px] mb-[18px]  mt-2'> <MdAdd className="text-2xl text-[#df0c12]"/>Add to cart</button>
+                             
                 </div>
+                
               );
             })}
+          
           </section>
+          <Link to='/product' className="text-center text-[#df0c12] mt-2">View All Products</Link>
+</div>
+    
     </div>
   );
 };
